9115a3f0dd9e945fea9a5c8dc4b404d84e197918
Your Name <you@example.com>
2023-10-27 20:13:23 -0300

    27/10/23

--- app/Views/subirvuelos.php
+++ app/Views/subirvuelos.php
@@ -2,9 +2,25 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aereoz</title>
+    <link rel="stylesheet" href="<?= base_url ("style.css")?>"> <!-- Enlaza tu archivo de estilos CSS -->
+    <link rel="icon" href="<?php echo base_url("/image/logoo.png") ?>" type="image/x-icon">
 </head>
 <body>
+<header class="main-header">
+        <div class="container">
+            <div class="logo">
+                <img src="<?php echo base_url("/image/logon.png")?>" alt="Aereoz Logo">
+            </div>
+            <nav class="main-nav">
+                <ul>
+                    <li><a href></a></li>
+                    <!-- Agrega más enlaces de navegación aquí -->
+                </ul>
+            </nav>
+        </div>
+    </header>
     <h2>Subir Vuelo</h2>
     <form method="post" action="<?= base_url('VueloController/subirvuelos'); ?>">
     <label for="destino">Destino</label>
